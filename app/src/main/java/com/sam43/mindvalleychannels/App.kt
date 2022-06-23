2a124150919778d7a620fce4f1d29b0cf8f4122c
Sayem <asm.sayem@bongobd.com>
2022-06-23 15:15:21 +0600

    Creating initial project

---
+++ app/src/main/java/com/sam43/mindvalleychannels/App.kt
@@ -0,0 +1,5 @@
+package com.sam43.mindvalleychannels
+
+import android.app.Application
+
+class App: Application()
---
+++ app/src/main/java/com/sam43/mindvalleychannels/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.sam43.mindvalleychannels.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
