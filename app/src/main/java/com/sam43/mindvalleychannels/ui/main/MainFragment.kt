622a1b32fad2f3abd5df94639da4a1c418fd1946
Sayem <asm.sayem@bongobd.com>
2022-06-23 16:17:10 +0600

    feature/config-arch-components - adding hilt (DI) impl and Runner Class - view binding implemented

--- app/src/main/java/com/sam43/mindvalleychannels/ui/main/MainFragment.kt
+++ app/src/main/java/com/sam43/mindvalleychannels/ui/main/MainFragment.kt
@@ -6,27 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.sam43.mindvalleychannels.R
+import com.sam43.mindvalleychannels.databinding.MainFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-
     companion object {
         fun newInstance() = MainFragment()
     }
-
-    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
