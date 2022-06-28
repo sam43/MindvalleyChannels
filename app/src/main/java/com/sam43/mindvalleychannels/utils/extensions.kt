16f2b5acbe6d35bfe12039658bf544794a006f11
Sayem <asm.sayem@bongobd.com>
2022-06-28 18:15:07 +0600

    feature/unit-ui-testing - ui fix (3) and json response added for testing

--- app/src/main/java/com/sam43/mindvalleychannels/utils/extensions.kt
+++ app/src/main/java/com/sam43/mindvalleychannels/utils/extensions.kt
@@ -6,6 +6,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.snackbar.Snackbar
 import com.sam43.mindvalleychannels.R
@@ -13,6 +14,8 @@ import com.sam43.mindvalleychannels.R
 fun loadImage(iv: ImageView, url: String?, placeholder: Int = R.drawable.placeholder) =
     Glide.with(iv.context)
         .load(url)
+        .onlyRetrieveFromCache(true)
+        .transition(DrawableTransitionOptions.withCrossFade())
         .centerCrop()
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .placeholder(placeholder)
@@ -21,7 +24,8 @@ fun loadImage(iv: ImageView, url: String?, placeholder: Int = R.drawable.placeho
 fun loadImageCircular(iv: ImageView, url: String?, placeholder: Int = R.drawable.ic_icon_asset_circular) =
     Glide.with(iv.context)
         .load(url)
-        .dontAnimate()
+        .onlyRetrieveFromCache(true)
+        .transition(DrawableTransitionOptions.withCrossFade())
         .apply(RequestOptions.circleCropTransform())
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .placeholder(placeholder)
